095f17fc103656b9b1a37fff8d35b7b126b13b36
gabrieletodaroo9 <gabrieletodaroo9@gmail.com>
2025-10-17 14:16:30 +0200

    add the array for the first snack

---
+++ assets/js/main.js
@@ -0,0 +1,44 @@
+// Snack 1
+// Creare un array di oggetti:
+// Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+// Stampare a schermo la bici con peso minore.
+
+// Creo un'array con dentro nome e peso 
+
+const bikes = [
+    {
+        name: "Fausto",
+        weight: 7.9
+    },
+    {
+        name:"Pino",
+        weight: 7.5
+    },
+    {
+        name:"Gino",
+        weight: 8.3
+    },
+    {
+        name:"Tino",
+        weight: 6.8
+    },
+    {
+        name:"Filippo",
+        weight: 6.6
+    },
+    {
+        name:"Mario",
+        weight: 9.2
+    },
+] 
+
+
+
+
+// Snack2**
+// Creare un array di oggetti di squadre di calcio.
+// Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+// Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+// Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
+// Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+// >
